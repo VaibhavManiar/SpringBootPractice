ede00c893fb3303ebb966b84435f138860d2705e
vaibhavmaniar <vaibhav.maniar@cleartrip.com>
2020-03-01 16:45:48 +0530

    Commit with only weather report

--- web/src/main/java/me/practice/springboot/SpringBootPracticeApplication.java
+++ web/src/main/java/me/practice/springboot/SpringBootPracticeApplication.java
@@ -1,12 +1,11 @@
 package me.practice.springboot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 /*@SpringBootApplication
-@ComponentScan("me.practice.springboot")
-@ComponentScan("com.weatherreport")*/
+@ComponentScan("me.practice.clea")*/
+@ComponentScan("com.weatherreport")
 public class SpringBootPracticeApplication {
 
 	public static void main(String[] args) {
